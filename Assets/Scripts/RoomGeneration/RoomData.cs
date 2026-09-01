972a168ab76f969823ee41e4f1b791a78f1d2024
BBurtz <117851173+BBurtz@users.noreply.github.com>
2026-09-01 08:56:16 -0500

    room prefabs made

---
+++ Assets/Scripts/RoomGeneration/RoomData.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomData : MonoBehaviour
+{
+    public int RoomCost;
+    public List<GameObject> RoomDoors;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
